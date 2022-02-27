f896d7a2e590ea68d34dfaa88e9ab0eecf25e715
SamuelThulin <68923807+SamuelThulin@users.noreply.github.com>
2022-02-27 16:03:11 -0500

    Update ref file for BG

--- js/sketch.js
+++ js/sketch.js
@@ -31,7 +31,7 @@ let bgInfo;
 // preload table data
 function preload() {
   dataBG = loadTable(
-    'BG_16Jul2021week.csv',
+    'BG_ContourCSVReport_2022_27_02_BG27FebweekHR',
     'csv',
     'header');
   hussar = loadFont("Hussar.otf");
